fa83b82da8c8b0282786b50afcf6481b931cc8eb
giorgia-nadizar <giorgia.nadizar@gmail.com>
2023-11-16 17:04:11 +0000

    Bi-mapelites added.

--- qdax/core/bi_map_elites.py
+++ qdax/core/bi_map_elites.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from functools import partial
-from typing import Callable, Optional, Tuple, Any
+from typing import Callable, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
 
-from qdax.core.containers.mapelites_repertoire import MapElitesRepertoire
+from qdax.core.containers.mapelites_bi_repertoire import MapElitesBiRepertoire
 from qdax.core.emitters.emitter import Emitter, EmitterState
 from qdax.core.map_elites import MAPElites
 from qdax.types import (
@@ -20,54 +20,80 @@ from qdax.types import (
 )
 
 
-class DoubleRepME(MAPElites):
+def sampling_function(function_name: str) -> Callable[[MapElitesBiRepertoire], int]:
+    if "0" in function_name or "both" in function_name:
+        return lambda x: 0
+    elif "1" in function_name:
+        return lambda x: 1
+    elif "2" in function_name:
+        return lambda x: 2
+    elif "most_covered" in function_name:
+        def _most_covered_sampler(bi_repertoire: MapElitesBiRepertoire) -> int:
+            repertoire_empty1 = bi_repertoire.repertoire1.fitnesses == -jnp.inf
+            coverage1 = 100 * jnp.mean(1.0 - repertoire_empty1)
+            repertoire_empty2 = bi_repertoire.repertoire2.fitnesses == -jnp.inf
+            coverage2 = 100 * jnp.mean(1.0 - repertoire_empty2)
+            return 1 + (coverage1 < coverage2)
+
+        return _most_covered_sampler
+    elif "least_covered" in function_name:
+        def _least_covered_sampler(bi_repertoire: MapElitesBiRepertoire) -> int:
+            repertoire_empty1 = bi_repertoire.repertoire1.fitnesses == -jnp.inf
+            coverage1 = 100 * jnp.mean(1.0 - repertoire_empty1)
+            repertoire_empty2 = bi_repertoire.repertoire2.fitnesses == -jnp.inf
+            coverage2 = 100 * jnp.mean(1.0 - repertoire_empty2)
+            return 1 + (coverage1 > coverage2)
+
+        return _least_covered_sampler
+    else:
+        raise ValueError("Solver must be either cgp or lgp.")
+
+
+class BiMAPElites(MAPElites):
     def __init__(
         self,
         scoring_function: Callable[
             [Genotype, RNGKey], Tuple[Fitness, Descriptor, ExtraScores, RNGKey]
         ],
         emitter: Emitter,
-        metrics_function: Callable[[MapElitesRepertoire], Metrics],
-        n_descriptors: int,
-        extra_descriptors_indexes: jnp.ndarray = jnp.asarray([]),
-        extra_metrics_functions: Callable[[MapElitesRepertoire], Metrics] = lambda x: {}
+        metrics_function: Callable[[MapElitesBiRepertoire], Metrics],
+        descriptors_indexes1: jnp.ndarray,
+        descriptors_indexes2: jnp.ndarray = jnp.asarray([]),
+        sampling_id_function: Callable[[MapElitesBiRepertoire], int] = lambda x: 0
+
+        # Note: sampling id semantics
+        # 0 = sample from both
+        # 1 = sample from first
+        # 2 = sample from second
     ) -> None:
-        super(DoubleRepME, self).__init__(scoring_function, emitter, metrics_function)
-        self._main_descriptors_indexes = jnp.setdiff1d(jnp.arange(n_descriptors), extra_descriptors_indexes)
-        self._extra_descriptors_indexes = extra_descriptors_indexes
-        self._extra_metrics_functions = extra_metrics_functions
+        super(BiMAPElites, self).__init__(scoring_function, emitter, metrics_function)
+        self._descriptors_indexes1 = descriptors_indexes1
+        self._descriptors_indexes2 = descriptors_indexes2
+        self._sampling_id_function = sampling_id_function
 
     @partial(jax.jit, static_argnames=("self",))
     def init(
         self,
         init_genotypes: Genotype,
-        main_centroids: Centroid,
-        extra_centroids: Centroid,
+        centroids1: Centroid,
+        centroids2: Centroid,
         random_key: RNGKey,
-    ) -> Tuple[Tuple[MapElitesRepertoire, MapElitesRepertoire], Optional[EmitterState], RNGKey]:
+    ) -> Tuple[MapElitesBiRepertoire, Optional[EmitterState], RNGKey]:
         # score initial genotypes
         fitnesses, descriptors, extra_scores, random_key = self._scoring_function(
             init_genotypes, random_key
         )
 
-        # split descriptors into main and extra
-        main_descriptors = descriptors.take(self._main_descriptors_indexes, axis=1)
-        extra_descriptors = descriptors.take(self._extra_descriptors_indexes, axis=1)
-
-        # init the repertoire
-        main_repertoire = MapElitesRepertoire.init(
+        # init the bi-repertoire
+        bi_repertoire = MapElitesBiRepertoire.init(
             genotypes=init_genotypes,
             fitnesses=fitnesses,
-            descriptors=main_descriptors,
-            centroids=main_centroids,
-            extra_scores=extra_scores,
-        )
-        extra_repertoire = MapElitesRepertoire.init(
-            genotypes=init_genotypes,
-            fitnesses=fitnesses,
-            descriptors=extra_descriptors,
-            centroids=extra_centroids,
-            extra_scores=extra_scores,
+            descriptors=descriptors,
+            descriptors_indexes1=self._descriptors_indexes1,
+            descriptors_indexes2=self._descriptors_indexes2,
+            centroids1=centroids1,
+            centroids2=centroids2,
+            extra_scores=extra_scores
         )
 
         # get initial state of the emitter
@@ -78,80 +104,48 @@ class DoubleRepME(MAPElites):
         # update emitter state
         emitter_state = self._emitter.state_update(
             emitter_state=emitter_state,
-            repertoire=main_repertoire,
+            repertoire=bi_repertoire,
             genotypes=init_genotypes,
             fitnesses=fitnesses,
-            descriptors=main_descriptors,
+            descriptors=descriptors,
             extra_scores=extra_scores,
         )
 
-        return (main_repertoire, extra_repertoire), emitter_state, random_key
+        return bi_repertoire, emitter_state, random_key
 
     @partial(jax.jit, static_argnames=("self",))
     def update(
         self,
-        repertoires: Tuple[MapElitesRepertoire, MapElitesRepertoire],
+        bi_repertoire: MapElitesBiRepertoire,
         emitter_state: Optional[EmitterState],
         random_key: RNGKey,
-    ) -> Tuple[Tuple[MapElitesRepertoire, MapElitesRepertoire], Optional[EmitterState], Metrics, RNGKey]:
+    ) -> Tuple[MapElitesBiRepertoire, Optional[EmitterState], Metrics, RNGKey]:
+        sampling_id = self._sampling_id_function(bi_repertoire)
+        bi_repertoire = bi_repertoire.update_sampling_mask(sampling_id)
+
         # generate offsprings with the emitter
-        main_repertoire, extra_repertoire = repertoires
-        genotypes, random_key = self._emitter.emit(
-            main_repertoire, emitter_state, random_key
+        offspring, random_key = self._emitter.emit(
+            bi_repertoire, emitter_state, random_key
         )
         # scores the offspring
         fitnesses, descriptors, extra_scores, random_key = self._scoring_function(
-            genotypes, random_key
+            offspring, random_key
         )
 
-        # split descriptors into main and extra
-        main_descriptors = descriptors.take(self._main_descriptors_indexes, axis=1)
-        extra_descriptors = descriptors.take(self._extra_descriptors_indexes, axis=1)
-
         # add genotypes in the repertoire
-        main_repertoire = main_repertoire.add(genotypes, main_descriptors, fitnesses, extra_scores)
-        extra_repertoire = extra_repertoire.add(genotypes, extra_descriptors, fitnesses, extra_scores)
+        bi_repertoire, _ = bi_repertoire.add(offspring, descriptors, fitnesses, extra_scores)
 
         # update emitter state after scoring is made
         emitter_state = self._emitter.state_update(
             emitter_state=emitter_state,
-            repertoire=main_repertoire,
-            genotypes=genotypes,
+            repertoire=bi_repertoire,
+            genotypes=offspring,
             fitnesses=fitnesses,
-            descriptors=main_descriptors,
+            descriptors=descriptors,
             extra_scores=extra_scores,
         )
 
         # update the metrics
-        main_metrics = self._metrics_function(main_repertoire)
-        extra_metrics = self._extra_metrics_functions(extra_repertoire)
-        # extra_metrics = jax.tree_map(lambda k: "extra_" + k, extra_metrics)
-        metrics = {**main_metrics, **extra_metrics}
+        metrics = self._metrics_function(bi_repertoire)
 
-        return (main_repertoire, extra_repertoire), emitter_state, metrics, random_key
-
-    @partial(jax.jit, static_argnames=("self",))
-    def scan_update(
-        self,
-        carry: Tuple[Tuple[MapElitesRepertoire, MapElitesRepertoire], Optional[EmitterState], RNGKey],
-        unused: Any,
-    ) -> Tuple[Tuple[Tuple[MapElitesRepertoire, MapElitesRepertoire], Optional[EmitterState], RNGKey], Metrics]:
-        """Rewrites the update function in a way that makes it compatible with the
-        jax.lax.scan primitive.
-
-        Args:
-            carry: a tuple containing the repertoire, the emitter state and a
-                random key.
-            unused: unused element, necessary to respect jax.lax.scan API.
-
-        Returns:
-            The updated repertoire and emitter state, with a new random key and metrics.
-        """
-        repertoires, emitter_state, random_key = carry
-        (repertoires, emitter_state, metrics, random_key,) = self.update(
-            repertoires,
-            emitter_state,
-            random_key,
-        )
-
-        return (repertoires, emitter_state, random_key), metrics
+        return bi_repertoire, emitter_state, metrics, random_key
