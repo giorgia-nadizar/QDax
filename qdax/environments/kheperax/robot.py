f5d5a0c3c9714c0a2d456a46ad338084793427cc
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-02-13 11:57:28 +0100

    Code for evolving sight sensors placement

--- qdax/environments/kheperax/robot.py
+++ qdax/environments/kheperax/robot.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
-from typing import Union, Tuple
+from functools import partial
+from typing import Union, Tuple, Callable
 
 import flax.struct
 import jax.lax
 import jax.tree_util
-from jax import numpy as jnp
+from brax.jumpy import cond
+from jax import numpy as jnp, vmap
 
 from qdax.environments.kheperax.geoms import Pos, Disk, Segment
 from qdax.environments.kheperax.laser import Laser
@@ -108,3 +110,67 @@ class Robot:
     def bumper_measures(self, maze: Maze) -> jnp.ndarray:
         get_measure_v = jax.vmap(lambda segment: self.bumper_measure_with_segment(segment))
         return jnp.max(get_measure_v(maze.walls), axis=0)
+
+
+@flax.struct.dataclass
+class SightEvolvableRobot(Robot):
+    desired_laser_angles: jnp.ndarray = jnp.array([])
+
+    @staticmethod
+    def _interpolate(y_l: float, y_r: float, x_l: float, x_r: float, x: float) -> float:
+        return (x - x_r) * (y_l - y_r) / (x_l - x_r) + y_r
+
+    @staticmethod
+    def _single_angle_interpolation(
+            target_angle: float,
+            sight_readings: jnp.ndarray,
+            real_angles: jnp.ndarray) -> float:
+        idx_l = jnp.searchsorted(real_angles, target_angle, side='right')
+        idx_r = jnp.searchsorted(real_angles, target_angle, side='right') + 1
+        real_angles_processed = jnp.concatenate([
+            jnp.asarray([real_angles[len(real_angles) - 1] - 2 * jnp.pi]),
+            real_angles,
+            jnp.asarray([real_angles[0] + 2 * jnp.pi])
+        ])
+        sight_readings_processed = jnp.concatenate([
+            jnp.asarray([sight_readings[len(sight_readings) - 1]]),
+            sight_readings,
+            jnp.asarray([sight_readings[0]])
+        ])
+        x_l, x_r = real_angles_processed[idx_l], real_angles_processed[idx_r]
+        y_l, y_r = sight_readings_processed[idx_l], sight_readings_processed[idx_r]
+        return SightEvolvableRobot._interpolate(y_l, y_r, x_l, x_r, target_angle)
+
+    @classmethod
+    def from_robot_and_genotype(cls, robot: Robot,
+                                genotype: jnp.ndarray,
+                                desired_laser_angles: jnp.ndarray) -> SightEvolvableRobot:
+        real_laser_angles = jnp.tanh(genotype) * jnp.pi  # defined in [-pi, pi]
+        return SightEvolvableRobot.from_robot(robot, real_laser_angles, desired_laser_angles)
+
+    @classmethod
+    def from_robot(cls,
+                   robot: Robot,
+                   real_laser_angles: jnp.ndarray,
+                   desired_laser_angles: jnp.ndarray
+                   ) -> SightEvolvableRobot:
+        desired_laser_angles = jnp.sort(desired_laser_angles)
+        real_laser_angles = jnp.sort(real_laser_angles)
+        return cls(
+            posture=robot.posture,
+            radius=robot.radius,
+            range_lasers=robot.range_lasers,
+            laser_angles=real_laser_angles,
+            std_noise_sensor_measures=robot.std_noise_sensor_measures,
+            lasers_return_minus_one_if_out_of_range=robot.lasers_return_minus_one_if_out_of_range,
+            desired_laser_angles=desired_laser_angles,
+        )
+
+    # the values returned are obtained from the actual ones via interpolation, as if perceived from the desired ones
+    def laser_measures(self, maze: Maze, random_key) -> jnp.ndarray:
+        real_measures = super().laser_measures(maze, random_key)
+        single_angle_interpolation_func = partial(SightEvolvableRobot._single_angle_interpolation,
+                                                  sight_readings=real_measures,
+                                                  real_angles=self.laser_angles,
+                                                  )
+        return vmap(single_angle_interpolation_func)(self.desired_laser_angles)
