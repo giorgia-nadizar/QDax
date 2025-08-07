68b19c0784285a59cc7618ee1a3b9350cd4e39aa
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-08-07 16:43:45 +0200

    apply function defined to operate as NN module

--- qdax/core/graphs/cartesian_genetic_programming.py
+++ qdax/core/graphs/cartesian_genetic_programming.py
@@ -4,7 +4,8 @@ import jax.numpy as jnp
 from chex import PRNGKey
 from flax import struct
 from flax.typing import RNGSequences, FrozenVariableDict
-from jax import random
+from jax import random, jit
+from jax._src.lax.control_flow import fori_loop
 
 from qdax.core.graphs.functions import FunctionSet
 
@@ -43,3 +44,34 @@ class CGP:
                 jnp.floor(random_out * out_mask).astype(int)
             }
         }
+
+    def apply(self,
+              cgp_genome_params: Union[FrozenVariableDict, Dict[str, Any]],
+              obs: jnp.ndarray,
+              ) -> jnp.ndarray:
+        # define function to update buffer in a certain position: get inputs from the x and y connections, then apply the function
+        @jit
+        def _update_buffer(buffer_idx: int,
+                           carry: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]) \
+                -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+            x_genes, y_genes, f_genes, buff = carry
+            n_in = len(buff) - len(x_genes)
+            idx = buffer_idx - n_in
+            f_idx = f_genes.at[idx].get()
+            x_arg = buff.at[x_genes.at[idx].get()].get()
+            y_arg = buff.at[y_genes.at[idx].get()].get()
+            f_computed = self.function_set.apply(f_idx, x_arg, y_arg)
+            buff = buff.at[buffer_idx].set(f_computed)
+            return x_genes, y_genes, f_genes, buff
+
+        # initialize the buffer with inputs and constants and use zeros as placeholders for computation
+        buffer = jnp.concatenate([obs, self.input_constants, jnp.zeros(self.n_nodes)])
+        # apply the buffer update function for all positions of the buffer to update it
+        _, _, _, buffer = fori_loop(self.n_inputs, len(buffer), _update_buffer,
+                                    (cgp_genome_params["params"]["x_connections_genes"],
+                                     cgp_genome_params["params"]["y_connections_genes"],
+                                     cgp_genome_params["params"]["functions_genes"], buffer))
+        outputs = jnp.take(buffer, cgp_genome_params["params"]["output_connections_genes"])
+
+        # apply wrapper to constraint the outputs in the correct domain
+        return self.outputs_wrapper(outputs)
